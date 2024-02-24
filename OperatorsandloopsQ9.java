97b0d1c9342724de007468bfbe27d92f5734b6e3
hellayush <129208635+hellayush@users.noreply.github.com>
2024-02-24 13:42:11 +0530

    Operators and Loops Assignment  has been added

---
+++ OperatorsandloopsQ9.java
@@ -0,0 +1,14 @@
+package InputCode.Assignment_output;
+
+public class OperatorsandLoopsAssignment {
+
+        public static void main(String[] args) {
+            // Using a for loop to print numbers from 1 to 10
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+        }
+    }
+    
+    
+
